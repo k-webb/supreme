83718df98979deab2569adb044e120ed21874679
Kyle Webb <k-webb@users.noreply.github.com>
2017-09-04 16:05:09 -0500

    Delete menu.py

--- menu.py
+++
@@ -1,9 +0,0 @@
-
-
-print ("\nCategories:\n")
-
-count = 0
-for item in data[u'products_and_categories']:
-    print(str(count)+' '+str(item))
-    count+=1
-choice = raw_input('What Catagory? (1-10)')
